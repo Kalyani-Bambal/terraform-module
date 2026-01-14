a3b30c66aa49c95382be9a8c073b716cdbd64e50
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2026-01-14 16:54:50 +0000

    update the file

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -15,3 +15,22 @@ resource "aws_iam_role_policy_attachment" "cluster_policy" {
   role = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+
+resource "aws_eks_cluster" "this" {
+  name = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version = var.cluster_version
+
+  vpc_config {
+    subnet_ids = var.private_subnet_ids
+    endpoint_private_access = true
+    endpoint_public_access = false
+  }
+
+  tags = merge (
+    var.tags,
+    {
+        Name = var.cluster_name
+    }
+  )
+}
--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,3 +1,19 @@
 variable "env" {
   type = string
 }
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_version" {
+  type = string
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "tags" {
+  type = map(string)
+}
