21266aadef60b60654a8005de284245228336661
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2026-01-14 16:42:00 +0000

    adding EKS module along with IAM role

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,18 +1,17 @@
 resource "aws_iam_role" "eks_cluster_role" {
   name = "${var.env}-eks-cluster-role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+        Effect = "Allow"
+        Principal = {service = "eks.amazonaws.com"}
+        Action = "sts:AssumeRole"
+    }]
+  })
 }
-EOF
+
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
+  role = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
