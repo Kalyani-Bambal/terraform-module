5189abc31f87bf7251b71b19b9fe7528db1122f1
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2026-01-20 08:39:42 +0000

    file add

--- modules/eks/aws-auth.tf
+++ modules/eks/aws-auth.tf
@@ -1,27 +1,3 @@
-################################
-# Kubernetes Provider (EKS)
-################################
-provider "kubernetes" {
-  host                   = aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(
-    aws_eks_cluster.this.certificate_authority[0].data
-  )
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args = [
-      "eks",
-      "get-token",
-      "--cluster-name",
-      aws_eks_cluster.this.name
-    ]
-  }
-}
-
-################################
-# aws-auth ConfigMap
-################################
 resource "kubernetes_config_map" "aws_auth" {
   metadata {
     name      = "aws-auth"
@@ -31,14 +7,7 @@ resource "kubernetes_config_map" "aws_auth" {
   data = {
     mapUsers = <<YAML
 - userarn: arn:aws:iam::358871393576:user/Kalyani-Bambal
-  username: Kalyani-Bambal
-  groups:
-    - system:masters
-YAML
-
-    mapRoles = <<YAML
-- rolearn: arn:aws:iam::358871393576:role/github-actions-terraform-role-1
-  username: github-actions
+  username: kalyani-bambal
   groups:
     - system:masters
 YAML
