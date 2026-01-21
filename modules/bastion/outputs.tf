78eb6d8dea77abdc91c6fc08ca5db4a39c4ffd14
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2026-01-21 12:51:11 +0000

    add

--- modules/bastion/outputs.tf
+++ modules/bastion/outputs.tf
@@ -3,6 +3,6 @@ output "public_ip_address" {
   value = aws_instance.bastion.public_ip
 }
 
-# output "bastion_security_group_ids" {
-#   value = aws_security_group.bastion_sg.id
-# }
+output "bastion_security_group_ids" {
+  value = aws_security_group.bastion_sg.id
+}
