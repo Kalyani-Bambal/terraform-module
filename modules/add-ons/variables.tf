999662724481e79185f5e2820c961c912b82c5e8
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2026-01-25 16:42:01 +0000

    test

--- modules/add-ons/variables.tf
+++ modules/add-ons/variables.tf
@@ -14,10 +14,10 @@ variable "kube_proxy_version" {
   type = string
 }
 
-variable "ebs_csi_driver_version" {
-  type = string
-}
+# variable "ebs_csi_driver_version" {
+#   type = string
+# }
 
-variable "efs_csi_driver_version" {
-  type = string
-}
+# variable "efs_csi_driver_version" {
+#   type = string
+# }
