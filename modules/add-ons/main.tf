273bfa8cb8e9ec93be6ce9588348f80ec660b971
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2026-01-25 07:40:32 +0000

    added new resources

--- modules/add-ons/main.tf
+++ modules/add-ons/main.tf
@@ -24,3 +24,23 @@ resource "aws_eks_addon" "kube_proxy" {
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 }
+
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name  = var.cluster_name
+  addon_name    = "aws-ebs-csi-driver"
+  addon_version = var.ebs_csi_driver_version
+
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+  
+}
+
+resource "aws_eks_addon" "efs_csi_driver" {
+  cluster_name  = var.cluster_name
+  addon_name    = "aws-efs-csi-driver"
+  # No version specified, uses latest available
+
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+  
+}
